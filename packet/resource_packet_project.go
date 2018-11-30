ae2f353b276226d509f65f29db7b401d4f8a9f8f
Tomas Karasek <tom.to.the.k@gmail.com>
2018-11-30 12:07:38 +0200

    Add partial crete

--- packet/resource_packet_project.go
+++ packet/resource_packet_project.go
@@ -155,12 +155,19 @@ func resourcePacketProjectRead(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	d.Set("id", proj.ID)
+	d.Partial(true)
+
+	d.SetId(proj.ID)
 	d.Set("payment_method_id", path.Base(proj.PaymentMethod.URL))
+	d.SetPartial("payment_method_id")
 	d.Set("name", proj.Name)
+	d.SetPartial("name")
 	d.Set("organization_id", path.Base(proj.Organization.URL))
+	d.SetPartial("organization_id")
 	d.Set("created", proj.Created)
+	d.SetPartial("created")
 	d.Set("updated", proj.Updated)
+	d.SetPartial("updated")
 
 	bgpConf, _, err := client.BGPConfig.Get(proj.ID, nil)
 	if err != nil {
@@ -177,6 +184,7 @@ func resourcePacketProjectRead(d *schema.ResourceData, meta interface{}) error {
 			}
 		}
 	}
+	d.Partial(false)
 	// empty : {}
 	return nil
 }
