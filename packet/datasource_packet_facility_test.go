b2a03bd6c8d352d73f3247fecc8c1b26994035dc
Tomas Karasek <tokarasek@gmail.com>
2018-12-14 17:56:55 +0200

    fix facility test, add skeleton of doc

--- packet/datasource_packet_facility_test.go
+++ packet/datasource_packet_facility_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestAccPacketFacility_Basic(t *testing.T) {
-	var totalFacs int
+	totalFacs := new(int)
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
@@ -19,86 +19,75 @@ func TestAccPacketFacility_Basic(t *testing.T) {
 			resource.TestStep{
 				Config: `data "packet_facility" "test" {}`,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckFacilityMeta("data.packet_facility.test", &totalFacs),
+					testAccCheckFacility("data.packet_facility.test", totalFacs),
 				),
 			},
 			resource.TestStep{
 				Config: `data "packet_facility" "test" { features = ["storage", "global_ipv4"] }`,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckFacilityLessThan("data.packet_facility.test", &totalFacs),
+					testAccCheckFacilityLessThan("data.packet_facility.test", totalFacs),
 				),
 			},
 		},
 	})
 }
 
+func checkFacilitiesAndGetCount(s *terraform.State, res string) (error, int) {
+	rs, ok := s.RootModule().Resources[res]
+	if !ok {
+		return fmt.Errorf("Can't find facilities data source: %s", res), 0
+	}
+
+	if rs.Primary.ID == "" {
+		return errors.New("facilities data source ID not set."), 0
+	}
+
+	countStr, ok := rs.Primary.Attributes["slugs.#"]
+	if !ok {
+		return fmt.Errorf("can't find 'slugs' attribute"), 0
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		return errors.New("failed to read number of facilities"), 0
+	}
+	if count == 0 {
+		return fmt.Errorf("expected some facilities listed, this is most likely a bug"), 0
+	}
+	for i := 0; i < count; i++ {
+		idx := "slugs." + strconv.Itoa(i)
+		v, ok := rs.Primary.Attributes[idx]
+		if !ok {
+			return fmt.Errorf("facilities list is corrupt (%q not found), this is definitely a bug", idx), 0
+		}
+		if len(v) < 1 {
+			return fmt.Errorf("Empty facility slug (%q), this is definitely a bug", idx), 0
+		}
+	}
+	return nil, count
+
+}
+
 func testAccCheckFacilityLessThan(res string, total *int) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[res]
-		if !ok {
-			return fmt.Errorf("Can't find regions data source: %s", res)
-		}
-
-		if rs.Primary.ID == "" {
-			return errors.New("regions data source ID not set.")
-		}
-
-		countStr, ok := rs.Primary.Attributes["slugs.#"]
-		if !ok {
-			return fmt.Errorf("can't find 'slugs' attribute")
-		}
-
-		count, err := strconv.Atoi(countStr)
+		err, count := checkFacilitiesAndGetCount(s, res)
 		if err != nil {
-			return errors.New("failed to read number of facilities")
+			return err
 		}
-		if count == 0 {
-			return fmt.Errorf("expected some facilities listed, this is most likely a bug")
-		}
-		return fmt.Errorf("%s should filter out some facilities, and the total should be less than %d, %d", res, total, count)
 		if count >= *total {
-			return fmt.Errorf("%s should filter out some facilities, and the total should be less than %d, %d", res, total, count)
+			return fmt.Errorf("%q should filter out some facilities, and the total should be less than %d, but is %d", res, *total, count)
 		}
-
 		return nil
 	}
 }
 
-func testAccCheckFacilityMeta(res string, total *int) resource.TestCheckFunc {
+func testAccCheckFacility(res string, total *int) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[res]
-		if !ok {
-			return fmt.Errorf("Can't find regions data source: %s", res)
-		}
-
-		if rs.Primary.ID == "" {
-			return errors.New("regions data source ID not set.")
-		}
-
-		countStr, ok := rs.Primary.Attributes["slugs.#"]
-		if !ok {
-			return fmt.Errorf("can't find 'slugs' attribute")
-		}
-
-		count, err := strconv.Atoi(countStr)
+		err, count := checkFacilitiesAndGetCount(s, res)
 		if err != nil {
-			return errors.New("failed to read number of facilities")
+			return err
 		}
 		*total = count
-		if count == 0 {
-			return fmt.Errorf("expected some facilities listed, this is most likely a bug")
-		}
-
-		for i := 0; i < count; i++ {
-			idx := "slugs." + strconv.Itoa(i)
-			v, ok := rs.Primary.Attributes[idx]
-			if !ok {
-				return fmt.Errorf("facilities list is corrupt (%q not found), this is definitely a bug", idx)
-			}
-			if len(v) < 1 {
-				return fmt.Errorf("Empty facility slug (%q), this is definitely a bug", idx)
-			}
-		}
 		return nil
 	}
 }
