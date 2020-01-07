2953a280c464f37ae337083063b7452be3b1207d
Tomas Karasek <tom.to.the.k@gmail.com>
2020-01-07 19:03:48 +0200

    add only test sweepers

--- packet/resource_packet_volume_test.go
+++ packet/resource_packet_volume_test.go
@@ -2,210 +2,55 @@ package packet
 
 import (
 	"fmt"
-	"testing"
+	"log"
+	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/packethost/packngo"
 )
 
-func TestAccPacketVolume_Basic(t *testing.T) {
-	var volume packngo.Volume
-
-	rs := acctest.RandString(10)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckPacketVolumeDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckPacketVolumeConfig_basic(rs),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &volume),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "plan", "storage_1"),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "billing_cycle", "hourly"),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "size", "100"),
-				),
-			},
-		},
+func init() {
+	resource.AddTestSweepers("packet_volume", &resource.Sweeper{
+		Name: "packet_volume",
+		F:    testSweepVolumes,
 	})
 }
 
-func TestAccPacketVolume_Update(t *testing.T) {
-	var volume, v1, v2, v3, v4 packngo.Volume
+func testSweepVolumes(region string) error {
+	log.Printf("[DEBUG] Sweeping volumes")
+	meta, err := sharedConfigForRegion(region)
+	if err != nil {
+		return fmt.Errorf("Error getting client for sweeping volumes: %s", err)
+	}
+	client := meta.(*packngo.Client)
 
-	rs := acctest.RandString(10)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckPacketVolumeDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 10, "descstr", "storage_1", true),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &volume),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "locked", "true"),
-				),
-			},
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 10, "descstr", "storage_1", false),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &v1),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "locked", "false"),
-					testAccCheckPacketSameVolume(t, &volume, &v1),
-				),
-			},
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 10, "descstr2", "storage_2", false),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &v2),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "description", "descstr2"),
-					testAccCheckPacketSameVolume(t, &volume, &v2),
-				),
-			},
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 20, "descstr2", "storage_2", false),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &v3),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "size", "20"),
-					testAccCheckPacketSameVolume(t, &volume, &v3),
-				),
-			},
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 22, "descstr2", "storage_2", true),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &v4),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "locked", "true"),
-					testAccCheckPacketSameVolume(t, &volume, &v4),
-				),
-			},
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 25, "descstr2", "storage_2", false),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPacketVolumeExists("packet_volume.foobar", &v4),
-					resource.TestCheckResourceAttr(
-						"packet_volume.foobar", "locked", "false"),
-					testAccCheckPacketSameVolume(t, &volume, &v4),
-				),
-			},
-		},
-	})
-}
-
-func testAccCheckPacketVolumeDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*packngo.Client)
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "packet_volume" {
-			continue
-		}
-		if _, _, err := client.Volumes.Get(rs.Primary.ID, nil); err == nil {
-			return fmt.Errorf("Volume still exists")
+	ps, _, err := client.Projects.List(nil)
+	if err != nil {
+		return fmt.Errorf("Error getting project list for sweepeing volumes: %s", err)
+	}
+	pids := []string{}
+	for _, p := range ps {
+		if strings.HasPrefix(p.Name, "tfacc-") {
+			pids = append(pids, p.ID)
 		}
 	}
-
-	return nil
-}
-
-func testAccCheckPacketSameVolume(t *testing.T, before, after *packngo.Volume) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		if before.ID != after.ID {
-			t.Fatalf("Expected volume to be the same, but it was recreated: %s -> %s", before.ID, after.ID)
-		}
-		return nil
-	}
-}
-
-func TestAccPacketVolume_importBasic(t *testing.T) {
-	rs := acctest.RandString(10)
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckPacketVolumeDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckPacketVolumeConfig_var(rs, 10, "descstr", "storage_1", false),
-			},
-			{
-				ResourceName:      "packet_volume.foobar",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func testAccCheckPacketVolumeExists(n string, volume *packngo.Volume) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Record ID is set")
-		}
-
-		client := testAccProvider.Meta().(*packngo.Client)
-
-		foundVolume, _, err := client.Volumes.Get(rs.Primary.ID, nil)
+	vids := []string{}
+	for _, pid := range pids {
+		vs, _, err := client.Volumes.List(pid, nil)
 		if err != nil {
-			return err
+			return fmt.Errorf("Error listing volumes to sweep: %s", err)
 		}
-		if foundVolume.ID != rs.Primary.ID {
-			return fmt.Errorf("Record not found: %v - %v", rs.Primary.ID, foundVolume)
+		for _, v := range vs {
+			vids = append(vids, v.ID)
 		}
-
-		*volume = *foundVolume
-
-		return nil
 	}
-}
 
-func testAccCheckPacketVolumeConfig_basic(projectSuffix string) string {
-	return fmt.Sprintf(
-		`resource "packet_project" "foobar" {
-    name = "tfacc-volume-%s"
-}
-
-resource "packet_volume" "foobar" {
-    plan = "storage_1"
-    billing_cycle = "hourly"
-    size = 100
-    project_id = "${packet_project.foobar.id}"
-    facility = "ewr1"
-    snapshot_policies { 
-		snapshot_frequency = "1day"
-		snapshot_count = 7
+	for _, vid := range vids {
+		log.Printf("Removing volume %s", vid)
+		_, err := client.Volumes.Delete(vid)
+		if err != nil {
+			return fmt.Errorf("Error deleting volume %s", err)
+		}
 	}
-}`, projectSuffix)
-}
-
-func testAccCheckPacketVolumeConfig_var(projSuffix string, size int, desc string, planID string, locked bool) string {
-	return fmt.Sprintf(`
-resource "packet_project" "foobar" {
-    name = "tfacc-volume-%s"
-}
-
-resource "packet_volume" "foobar" {
-    billing_cycle = "hourly"
-    size = %d
-    description = "%s"
-    project_id = "${packet_project.foobar.id}"
-    facility = "ewr1"
-    plan = "%s"
-    locked = %t
-}
-`, projSuffix, size, desc, planID, locked)
+	return nil
 }
